fb65fe4e57d16809c06a6788e0c9c9707d30e81c
Filipe Fernandes <fernandes@MacBook-Air-de-Filipe.local>
2023-12-13 17:06:11 -0300

    Ultimas alterações relacionadas as formações da alura

---
+++ Lingaugem_CPP/Conhecendo_a_linguagem_C++_e_a_STL/jogodaadvinhacao.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "*************************************" << endl;
+    cout << "* Bem-vindos ao jogo da advinhação! *" << endl;
+    cout << "*************************************" << endl;
+    
+    int numero_secreto = 42;
+
+    cout << "O número secreto é " << numero_secreto << endl;
+}
