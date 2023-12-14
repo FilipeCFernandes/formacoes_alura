ed374bbb9018989df14892fef3ad39b91a969645
Fernandes <eng.fernandes_br@ieee.org>
2023-12-14 08:39:04 -0300

    Início do estudo em C++

--- Lingaugem_CPP/Conhecendo_a_linguagem_C++_e_a_STL/jogodaadvinhacao.cpp
+++ Lingaugem_CPP/Conhecendo_a_linguagem_C++_e_a_STL/jogodaadvinhacao.cpp
@@ -6,8 +6,31 @@ int main(){
     cout << "*************************************" << endl;
     cout << "* Bem-vindos ao jogo da advinhação! *" << endl;
     cout << "*************************************" << endl;
+    cout << endl;
     
-    int numero_secreto = 42;
+    const int NUMERO_SECRETO = 42;
 
-    cout << "O número secreto é " << numero_secreto << endl;
+    cout << "O número secreto é " << NUMERO_SECRETO<< endl;
+
+    int chute = 0;
+    bool acertou = chute == NUMERO_SECRETO;
+    bool maior = chute > NUMERO_SECRETO;
+
+    while(!acertou){
+        
+        cout << "Qual o seu chute? ";
+        cin >> chute; 
+        cout << endl << "O valor do seu chute é " << chute << endl;
+
+        maior = chute > NUMERO_SECRETO;
+        acertou = chute == NUMERO_SECRETO;
+
+        if (maior){
+            cout << "O seu chute é maior que o número secreto" << endl;
+        }else {
+            cout << "O seu chute é menor que o número secreto" << endl;
+        }
+    }
+
+    cout << " Você acertou o número secreto! ";
 }
