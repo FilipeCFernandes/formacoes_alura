c9cb3c019cebd1a4b6d78844921b8e6f3c52feba
Fernandes <eng.fernandes_br@ieee.org>
2023-12-18 11:27:10 -0300

    atualização do jogo da forca, agora as palavras são carregadas de uma lista e a lista pode ser atualizada dentro do próprio jogo

--- Lingaugem_CPP/Conhecendo_a_linguagem_C++_e_a_STL/forca.cpp
+++ Lingaugem_CPP/Conhecendo_a_linguagem_C++_e_a_STL/forca.cpp
@@ -2,11 +2,14 @@
 #include<string>
 #include<map>
 #include<vector>
+#include<fstream>
+#include<ctime>
+#include<cstdlib>
 
 using namespace std;
 
 // Definição de variáveis globais
-const string PALAVRA_SECRETA = "MELANCIA";
+string palavra_secreta;
 map<char, bool> chutou;
 vector<char> chutes_errados;
 
@@ -14,14 +17,14 @@ vector<char> chutes_errados;
 bool letra_existe(char chute){
 
 	// for na forma do C++ normal
-	//for(int i = 0; i < PALAVRA_SECRETA.size(); i++){
-	//	if(chute == PALAVRA_SECRETA[i]){
+	//for(int i = 0; i < palavra_secreta.size(); i++){
+	//	if(chute == palavra_secreta[i]){
 	//		return true;
 	//	}
 	//}
 
 	// for na forma do c++11
-	for(char letra : PALAVRA_SECRETA){
+	for(char letra : palavra_secreta){
 		if(chute == letra){
 			return true;
 		}
@@ -30,7 +33,7 @@ bool letra_existe(char chute){
 }
 
 bool nao_acertou(){
-	for(char letra : PALAVRA_SECRETA){
+	for(char letra : palavra_secreta){
 		if(!chutou[letra]){
 			return true;
 		}
@@ -42,15 +45,87 @@ bool nao_enforcou(){
 	return chutes_errados.size() < 5;
 }
 
-int main () {
-
-	cout << PALAVRA_SECRETA << endl;
-	
-//	bool nao_acertou = true;
-//	bool nao_enforcou = true;
+void imprime_cabecalho(){
 	cout << "***************************\n";
 	cout << "****** Jogo da forca ******\n";
 	cout << "***************************\n";
+}
+
+vector<string> le_arquivo(){
+	ifstream arquivo;
+	arquivo.open("palavras.txt");
+
+	if(arquivo.is_open()){
+	
+		int quantidade_de_palavras;
+		arquivo >> quantidade_de_palavras;
+	
+		string palavra; 
+		vector<string> lista_de_palavras;
+		for(int i = 0; i < quantidade_de_palavras ; i++){
+			arquivo >> palavra;
+			lista_de_palavras.push_back(palavra);
+		}
+	
+		// é uma boa prática sempre fechar o arquivo
+		arquivo.close();
+		return lista_de_palavras;
+	}
+	else{
+		cout << "Não foi possível acessar o banco de palavras";
+		exit(0);
+	}
+}
+
+void sorteia_palavra(){
+	vector<string> palavras = le_arquivo();
+
+	srand(time(NULL));
+	int indice_sorteado = rand() % palavras.size();
+	palavra_secreta = palavras[indice_sorteado];
+}
+
+void salva_arquivo(vector<string> nova_lista){
+	
+	ofstream arquivo;
+	arquivo.open("palavras.txt");
+	if(arquivo.is_open()){
+		arquivo << nova_lista.size() << endl;
+		for(string palavra : nova_lista){
+			arquivo << palavra << endl;
+		}
+
+		arquivo.close();
+	}
+	else{ 
+		cout << "Não foi possível acessar o banco de palavras";
+		exit(0);	
+	}
+}
+
+void adiciona_palavra(){
+	cout << "Digite a nova palavra, usando apenas letras maiúscula." << endl;
+	string nova_palavra;
+	cin >> nova_palavra;
+
+	vector<string> lista_de_palavras = le_arquivo();
+	lista_de_palavras.push_back(nova_palavra);
+
+	salva_arquivo(lista_de_palavras);
+}
+
+
+
+int main () {
+
+	imprime_cabecalho();
+
+	sorteia_palavra();
+//	cout << palavra_secreta << endl;
+	
+//	bool nao_acertou = true;
+//	bool nao_enforcou = true;
+	
 	while(nao_acertou() && nao_enforcou()){
 
 		cout << "Chutes errados: ";
@@ -59,7 +134,7 @@ int main () {
 		}
 		cout << endl; 
 
-		for(char letra : PALAVRA_SECRETA){
+		for(char letra : palavra_secreta){
 			if(chutou[letra]){
 				cout << letra << " ";
 			}else{
@@ -83,10 +158,19 @@ int main () {
 	
 	}
 	cout << "\n\n";
+	cout << "A palavra secreta é: " << palavra_secreta; 
+
 	if(!nao_enforcou()){
 		cout << "Você enforcou! Tente novamente";
 	}else{
-		cout << "Parabéns!!! Você acertou a palavra secreta!";
+		cout << "Parabéns!!! Você acertou a palavra secreta!\n";
+
+		cout << "Você deseja adicionar uma nova palavra ao banco? (S/N)\n";
+		char resposta;
+		cin >> resposta;
+		if (resposta == 'S'){
+			adiciona_palavra();
+		}
 	}
 
 }
